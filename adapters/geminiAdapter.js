3372f9f4b8cad0763249e9983ee42cc27a573707
Its-me-nishmal <dev.nishmal@gmail.com>
2025-06-24 17:52:59 +0530

    ok

--- adapters/geminiAdapter.js
+++ adapters/geminiAdapter.js
@@ -3,7 +3,7 @@ const config = require('../config/gemini');
 
 let key = config.apiKey
 const genAI = new GoogleGenerativeAI(config.apiKey);
-const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+const model = genAI.getGenerativeModel({ model: "Gemini 2.5 Flash-Lite Preview 06-17" });
 
 async function generateContent(prompt) {
   try {
--- adapters/geminiAdapterForDetection.js
+++ adapters/geminiAdapterForDetection.js
@@ -2,7 +2,7 @@ const { GoogleGenerativeAI } = require("@google/generative-ai");
 const config = require('../config/gemini');
 
 const genAI = new GoogleGenerativeAI(config.detectKey);
-const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+const model = genAI.getGenerativeModel({ model: "Gemini 2.5 Flash-Lite Preview 06-17" });
 
 async function generateContentForDetection(prompt) {
   try {
